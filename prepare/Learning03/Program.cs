80c363cac974c0ede5041a00b7da04e2568958cb
blakechr1454 <chr20026@byui.edu>
2024-02-26 00:45:48 -0700

    completed prep "Fraction"

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction blank = new Fraction();
+        Fraction wholeNum = new Fraction(5);
+        Fraction both = new Fraction(3, 4);
+        
+        Console.WriteLine($"{blank.GetFractionString()} {blank.GetDecimalValue()}");
+        Console.WriteLine($"{wholeNum.GetFractionString()} {wholeNum.GetDecimalValue()}");
+        Console.WriteLine($"{both.GetFractionString()} {both.GetDecimalValue()}");
     }
 }
