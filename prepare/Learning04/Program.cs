12451480f617c545144b2c6a1fdb1cc97c48007d
blakechr1454 <chr20026@byui.edu>
2024-03-04 14:38:45 -0700

    Completed Learning Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.Clear();
+
+        Assignment Frank = new Assignment("Frank Borris", "Math");
+        Frank.GetSummary();
+
+        Console.WriteLine();
+
+        MathAssignment Gary = new MathAssignment("Gary Oak", "Math", "7.3", "1-20");
+        Gary.GetSummary();
+        Gary.GetHomeworkList();
+
+        Console.WriteLine();
+
+        WritingAssignment Harry = new WritingAssignment("Harry Potter", "English", "The Sorcerer's Stone");
+        Harry.GetSummary();
+        Harry.GetWrittingInformation();
     }
 }
